1c7d97f13929a08b9d713273ca2e64dfbecb9a89
igooor4393 <109076361+igooor4393@users.noreply.github.com>
2022-09-15 17:26:23 +0300

    1

--- main.go
+++ main.go
@@ -5,27 +5,67 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"strconv"
 )
 
 func main() {
 
-	//s:=1
-
 	a := app.New()
-	w := a.NewWindow("Hello World")
+	w := a.NewWindow("Недокалькулятор GUI golang")
 
-	//:= widget.NewLabel(string(s)))
-	lable := widget.NewLabel("Привет")
+	//label := widget.NewLabel("Привет")
+	//entry := widget.NewEntry()
+	//btn := widget.NewButton("Push", func() {
+	//	data := entry.Text
+	//	fmt.Println(data)
+	//})
+	label := widget.NewLabel("enter number")
 	entry := widget.NewEntry()
-	btn := widget.NewButton("Push", func() {
-		data := entry.Text
-		fmt.Println(data)
+
+	label2 := widget.NewLabel("enter number")
+	entry2 := widget.NewEntry()
+
+	answer := widget.NewLabel("")
+
+	buttn := widget.NewButton("calculate", func() {
+		data1 := entry.Text
+		data2 := entry2.Text
+		fmt.Println(data1, data2)
+
+		numb1, er := strconv.ParseFloat(data1, 64)
+		numb2, err := strconv.ParseFloat(data2, 64)
+		if err != nil || er != nil {
+			answer.SetText("Ошибка ввода")
+		} else {
+			sum := numb1 + numb2
+			res := numb1 - numb2
+			mul := numb1 * numb2
+			div := numb1 / numb2
+
+			//sum1 := strconv.FormatFloat(sum, 'E', -1, 64)
+			//res1 := strconv.FormatFloat(res, 'E', -1, 64)
+			//mul1 := strconv.FormatFloat(mul, 'E', -1, 64)
+			//div1 := strconv.FormatFloat(div, 'E', -1, 64)
+
+			//answer.SetText("(+)" + sum1 + "/n" + "(-)" + res1 + "/n" + "(*)" + mul1 + "/n" + "(/)" + div1 + "/n")
+			answer.SetText(fmt.Sprintf("(+) %f\n (-) %f\n (*) %f\n (/) %f\n", sum, res, mul, div))
+
+		}
 	})
 
 	w.SetContent(container.NewVBox(
-		lable,
+		//label,
+		//entry,
+		//btn,
+		label,
 		entry,
-		btn,
+
+		label2,
+		entry2,
+
+		buttn,
+
+		answer,
 	))
 
 	w.ShowAndRun()
